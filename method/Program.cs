42ac9384a2249a2cd1fe89ad417d25eff2e537c9
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-06-14 00:08:23 +0400

    Add project files.

---
+++ method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace method
+{
+    public class Programs
+    {
+        public static void methodSqrt(int num)
+        {
+            var sqr = Math.Sqrt(num);
+            Console.WriteLine(sqr);
+        }
+        public static void methodPow(int num, int numTwo)
+        {
+            var sqr = Math.Pow(num, numTwo);
+            Console.WriteLine(sqr);
+        }
+        public static string userFullName(string fName, string lName)
+        {
+            var fullName = fName + " " + lName;
+            return fullName;
+        }
+    }
+}
---
+++ method/sqrtTest.cs
@@ -0,0 +1,7 @@
+﻿using method;
+
+
+Programs.methodSqrt(4);
+Programs.methodPow(4, 2);
+
+Console.WriteLine(Programs.userFullName("Mkrtich", "Manukyan"));
